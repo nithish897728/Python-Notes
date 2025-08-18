5b1fafae468ca7e85c8c5f6e7d484c68b5035f89
nithish897728 <nithishkoppala54@gmail.com>
2025-08-18 19:58:32 +0530

    18/08/2025

---
+++ assignment1.py
@@ -0,0 +1,25 @@
+# num=int(input("enter a number"))
+# print(num)
+
+#take input as your name print hello nithish
+
+# name='nithish'
+# print('hello',name)
+
+#take 2 numbers from  user input and print sum of 2 numbers
+
+# a=int(input("enyer the a number"))
+# b=int(input("enyer the a number"))
+# print('sum of 2 numbers',(a+b))
+
+#take 2 numbers from user input and print the product of 2 numbers
+
+# a=int(input('enter the a number'))
+# b=int(input("enyer the a number"))
+# print('product of 2 numbers',(a*b))
+
+#take age as a user input and print you are age years old
+
+a=int(input('enter your age'))
+print('you are',a,'years old')
+
